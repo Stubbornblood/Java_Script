1de4e3dad2cc07119de040f67487cde92bc10a87
Stubbornblood <ss3591419@gmail.com>
2022-03-05 22:34:40 +0530

    Function updaate v3

--- function.js
+++ function.js
@@ -1,5 +1,23 @@
+//function without parameter
 function SayHello(){
  console.log("Hello functions");
 }
 SayHello();
 
+//fundtion with parameter
+
+function sum(num1,num2){
+    let addition = num1+num2;
+    console.log(addition);
+}
+
+sum(3,5);
+
+//function with return type
+
+function multiply(num1,num2){
+    return num1*num2;
+}
+
+let ans = multiply(3,5);
+console.log(ans);
